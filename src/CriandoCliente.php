d27bdf082e4f064c5409095d3fa5c2ee278d351d
Anderson Andrade <anderson.carloscarvalho@gmail.com>
2023-10-14 16:36:48 -0300

    Tarefa 5 - Criando um cliente com dados básicos

---
+++ src/CriandoCliente.php
@@ -0,0 +1,21 @@
+<?php
+
+$cliente=[
+    'nome' => 'Anderson',
+    'email' => 'anderson@e-mail.com',
+    'telefone' =>
+        [
+            'movel' => '11989892525',
+            'residencial' => '1122582888'
+        ],
+    'endereco'  =>
+        [
+            'rua' => 'Rua do php, 33',
+            'complemento' => 'Casa',
+            'bairro' => 'laranjeiras',
+            'cidade' => 'são paulo',
+            'estado' => 'SP',
+            'cep' => '01721-200'
+        ]
+];
+
