c8122ece5f507d62acd72ab6aa98e553e36176a5
Anderson Andrade <139899277+AndersonCCandrade@users.noreply.github.com>
2023-10-28 19:47:34 -0300

    Revert "Semana 7"
    This reverts commit 2b5d2181a073f117b46d87079f9a59e8da49de9d.

--- src/index.php
+++ src/index.php
@@ -2,13 +2,16 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Andersonccandrade\Comex\Classes\{Cliente, Produto, Endereco, Pedido, Carrinho};
-use Andersonccandrade\Comex\Classes\Pagamento\{Pix,CartaoDeCredito,Boleto};
+use Andersonccandrade\Comex\Classes\Cliente;
+use Andersonccandrade\Comex\Classes\Produto;
+use Andersonccandrade\Comex\Classes\Endereco;
+use Andersonccandrade\Comex\Classes\Pedido;
+use Andersonccandrade\Comex\Classes\Carrinho;
 
 $clientes = [
     $cliente1 = new Cliente(
         'Anderson Carlos',
-        'anderson.ccarvalho@tr.spfac.br',
+        'anderson.ccarvalho@sp.senac.br',
         '11991500354',
         new Endereco(
             '02604200',
@@ -21,7 +24,7 @@ $clientes = [
 
     $cliente2 = new Cliente(
         'Davi Carvalho',
-        'davi.bcarvalho@tr.spfac.br',
+        'davi.bcarvalho@sp.senac.br',
         '11989895656',
         new Endereco(
             '02701200',
@@ -67,7 +70,7 @@ foreach ($produtos as $chave => $item){
 
 
 
-$pedido = new Pedido(1, $cliente1, $produtos,new Pix());
+$pedido = new Pedido(1, $cliente1, $produtos);
 
 
 echo "# Pedido n " . $pedido->getId() . PHP_EOL;
